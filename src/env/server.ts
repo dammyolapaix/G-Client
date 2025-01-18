b146884332ade070dfb533fd6dc461e8f006ac6e
Dammy Ola <dammyolapaix@gmail.com>
2025-01-18 21:25:31 +0000

    Added env config

---
+++ src/env/server.ts
@@ -0,0 +1,32 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { config } from 'dotenv'
+import { expand } from 'dotenv-expand'
+import { ZodError, z } from 'zod'
+
+expand(config())
+
+const stringBoolean = z
+  .string()
+  .refine((s) => s === 'true' || s === 'false')
+  .transform((s) => s === 'true')
+  .optional()
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(['development', 'production']),
+    DATABASE_URL: z.string().url(),
+    DB_MIGRATING: stringBoolean,
+    DB_SEEDING: stringBoolean,
+  },
+  isServer: typeof window === 'undefined',
+  emptyStringAsUndefined: false,
+  experimental__runtimeEnv: process.env,
+
+  onValidationError: (error: ZodError) => {
+    console.error(
+      '❌ Invalid environment variables:',
+      error.flatten().fieldErrors
+    )
+    process.exit(1)
+  },
+})
