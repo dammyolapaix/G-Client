3e409d7729be9df4145933b44b8699b8bc657960
Dammy Ola <dammyolapaix@gmail.com>
2025-01-20 16:25:08 +0000

    Hashed admin seed password

--- src/features/users/seed.ts
+++ src/features/users/seed.ts
@@ -1,3 +1,5 @@
+import { hash } from 'bcryptjs'
+
 import db from '@/db'
 import { users } from '@/db/schema'
 import { env } from '@/env/server'
@@ -5,10 +7,12 @@ import { env } from '@/env/server'
 import { InsertUser } from './types'
 
 export default async function seed() {
+  const password = await hash(env.ADMIN_PASSWORD, 10)
+
   const usersSeedData: InsertUser[] = [
     {
       email: env.ADMIN_EMAIL,
-      password: env.ADMIN_PASSWORD,
+      password,
       provider: 'email',
       role: 'admin',
       emailVerified: new Date().toISOString(),
