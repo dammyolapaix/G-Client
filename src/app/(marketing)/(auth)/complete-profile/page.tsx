bb39a65ec07f1f3ec875e262bd8db8c6f496c0c3
Dammy Ola <dammyolapaix@gmail.com>
2025-02-01 10:05:35 +0000

    Protect the CompleteProfilePage for non auth user, unverified email and profile completed

--- src/app/(marketing)/(auth)/complete-profile/page.tsx
+++ src/app/(marketing)/(auth)/complete-profile/page.tsx
@@ -1,6 +1,22 @@
+import { redirect } from 'next/navigation'
+
+import { UserWithRelationships } from '@/features/users/types'
+import auth from '@/lib/auth'
+import { DASHBOARD_ROUTE, LOGIN_ROUTE, VERIFY_EMAIL_ROUTE } from '@/lib/routes'
+
 import AuthLayout from '../_components/auth-layout'
 import { CompleteProfileForm } from '../_components/complete-profile-form'
 
-export default function CompletePage() {
+export default async function CompleteProfilePage() {
+  const authUser = (await auth.utils.getAuthUser({
+    with: { profile: true },
+  })) as UserWithRelationships
+
+  if (!authUser) redirect(LOGIN_ROUTE)
+
+  if (!authUser.emailVerified) redirect(VERIFY_EMAIL_ROUTE)
+
+  if (auth.utils.authUserProfileIsCompleted(authUser)) redirect(DASHBOARD_ROUTE)
+
   return <AuthLayout component={<CompleteProfileForm />} />
 }
