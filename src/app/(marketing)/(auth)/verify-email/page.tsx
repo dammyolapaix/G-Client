eb950003392684353ab96b3a3bd67801cc871498
Dammy Ola <dammyolapaix@gmail.com>
2025-02-01 10:06:41 +0000

    Rename verify email page function to VerifyEmailPage

--- src/app/(marketing)/(auth)/verify-email/page.tsx
+++ src/app/(marketing)/(auth)/verify-email/page.tsx
@@ -6,7 +6,7 @@ import { DASHBOARD_ROUTE, LOGIN_ROUTE } from '@/lib/routes'
 import AuthLayout from '../_components/auth-layout'
 import { VerifyEmailForm } from '../_components/verify-email-form'
 
-export default async function RegisterPage() {
+export default async function VerifyEmailPage() {
   const authUser = await auth.utils.getAuthUser()
 
   if (!authUser) redirect(LOGIN_ROUTE)
