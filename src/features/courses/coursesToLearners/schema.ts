7cdf7da90bc6cbbd4c0adb9a5033f149d3d259f7
Dammy Ola <dammyolapaix@gmail.com>
2025-02-02 10:01:16 +0000

    Update coursesToLearners schema

--- src/features/courses/coursesToLearners/schema.ts
+++ src/features/courses/coursesToLearners/schema.ts
@@ -1,5 +1,14 @@
 import { relations } from 'drizzle-orm'
-import { bigint, date, integer, pgTable, uuid } from 'drizzle-orm/pg-core'
+import {
+  bigint,
+  date,
+  integer,
+  pgTable,
+  primaryKey,
+  unique,
+  uuid,
+  varchar,
+} from 'drizzle-orm/pg-core'
 
 import { courses, users } from '@/db/schema'
 
@@ -9,19 +18,26 @@ import { courses, users } from '@/db/schema'
  * 2. The learners enrollment for a course
  */
 
-const coursesToLearners = pgTable('courses_to_learners', {
-  id: uuid().primaryKey().defaultRandom().notNull(),
-  courseId: uuid()
-    .references(() => courses.id, { onDelete: 'cascade' })
-    .notNull(),
-  learnerId: uuid()
-    .references(() => users.id, { onDelete: 'cascade' })
-    .notNull(),
-  amount: integer().notNull(),
-  date: date({ mode: 'string' }).defaultNow().notNull(),
-  paidAt: date(),
-  paystackTransactionId: bigint({ mode: 'number' }),
-})
+const coursesToLearners = pgTable(
+  'courses_to_learners',
+  {
+    courseId: uuid()
+      .references(() => courses.id, { onDelete: 'cascade' })
+      .notNull(),
+    learnerId: uuid()
+      .references(() => users.id, { onDelete: 'cascade' })
+      .notNull(),
+    amount: integer().notNull(),
+    date: date({ mode: 'string' }).defaultNow().notNull(),
+    paidAt: date(),
+    paystackReference: varchar({ length: 320 }).notNull().unique(),
+    paystackTransactionId: bigint({ mode: 'number' }),
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.courseId, t.learnerId] }),
+    unq: unique().on(t.courseId, t.learnerId),
+  })
+)
 
 export const coursesToLearnersRelations = relations(
   coursesToLearners,
