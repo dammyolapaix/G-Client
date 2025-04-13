e6c2fa257627f25fc1a0c5bdbd46f3ef87fa944f
Dammy Ola <dammyolapaix@gmail.com>
2025-04-13 22:32:44 +0000

    fix

--- src/components/email/verify-email.tsx
+++ src/components/email/verify-email.tsx
@@ -40,9 +40,9 @@ export default function VerifyEmail({ verificationCode }: Props) {
               <Heading style={h1}>Verify your email address</Heading>
               <Text style={mainText}>
                 Thanks for starting the new G-Client account creation process.
-                We want to make sure it's really you. Please enter the following
-                verification code when prompted. If you don&apos;t want to
-                create an account, you can ignore this message.
+                We want to make sure it&apos;s really you. Please enter the
+                following verification code when prompted. If you don&apos;t
+                want to create an account, you can ignore this message.
               </Text>
               <Section style={verificationSection}>
                 <Text style={verifyText}>Verification code</Text>
